e6e8fcff12ea199298ca88a590ec2551a9d7f2dc
jzhou <zhou0309@umn.edu>
2019-02-17 23:17:07 -0600

    add 27

--- 27_remove_element.java
+++ 27_remove_element.java
@@ -1,5 +1,30 @@
 class Solution {
   public int removeElement(int[] nums, int val) {
-    
+    if (nums.length == 0) {
+      return 0;
+    }
+    int start = 0;
+    int end = nums.length - 1;
+    int count = 0;
+    while (start <= end) {
+      while (nums[start] != val) {
+        count++;
+        start++;
+        if (start > end) {
+          return count;
+        }
+      }
+      while (nums[end] == val) {
+        end--;
+        if (start >= end) {
+          return count;
+        }
+      }
+      nums[start] = nums[end];
+      start++;
+      end--;
+        count++;
+    }
+    return count;
   }
 }
