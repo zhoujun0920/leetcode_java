4ba995d3c006b086ac13ddacbbe0caa7fae49dfb
jzhou <zhou0309@umn.edu>
2019-02-23 14:11:11 -0600

    add 122

--- 122_best_time_to_buy_and_sell_stock_ii.java
+++ 122_best_time_to_buy_and_sell_stock_ii.java
@@ -5,21 +5,22 @@ class Solution {
         return 0;
       }
       int profit = 0;
-      int low = -1;
-      int high = 1;
+      int low = 0;
+      int high = 0;
       for (int i = 1; i < n; i++) {
         if (prices[i] > prices[i - 1]) {
           high++;
-        } else {
-          if (low == -1) {
-            low = prices[i];
-          } else {
-            profit += high - low;
-            low = prices[i + 1];
-            high = 0;
+          if (high == n - 1) {
+            profit += prices[high] - prices[low];
+            return profit;
           }
+        } else {
+          profit += prices[high] - prices[low];
+          low = i;
+          high = i;
         }
       }
+
       return profit;
     }
 }
