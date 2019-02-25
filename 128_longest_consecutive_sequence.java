6dbaa32d26fd27ad3c360f908df1465d82f565c0
jzhou <zhou0309@umn.edu>
2019-02-24 22:27:06 -0600

    add 128

--- 128_longest_consecutive_sequence.java
+++ 128_longest_consecutive_sequence.java
@@ -46,3 +46,56 @@ class Solution {
         return i;
     }
 }
+
+//Still not good enough, not O(n)
+class Solution {
+    public int longestConsecutive(int[] nums) {
+      if (nums.length == 0) {
+        return 0;
+      }
+      TreeSet<Integer> s = new TreeSet<>();
+      for (int i : nums) {
+        s.add(i);
+      }
+      int count = 1;
+      int temp = 1;
+      int previous = s.pollFirst();
+      while (s.size() > 0) {
+        int current = s.pollFirst();
+        if (current == previous + 1) {
+          temp++;
+        } else {
+          temp = 1;
+        }
+        previous = current;
+        count = count > temp ? count : temp;
+      }
+      return count;
+    }
+}
+
+//!s.contains(i - 1) will save a lot of work
+class Solution {
+    public int longestConsecutive(int[] nums) {
+      if (nums.length <= 2) {
+        return 0;
+      }
+      Set<Integer> s = new HashSet<>();
+      for (int i : nums) {
+        s.add(i);
+      }
+      int count = 1;
+      for (int i : nums) {
+        if (!s.contains(i - 1)) {
+          int current = i;
+          int currentCount = 1;
+          while (s.contains(current + 1)) {
+            currentCount++;
+            current = i + 1;
+          }
+          count = count > currentCount ? count : currentCount;
+        }
+      }
+      return count;
+    }
+}
